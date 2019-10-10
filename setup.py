3d471b0941687f2d51c520e0f732dc7e17001048
Martin Nielsen <mbnielsen@bitbucket.org>
2019-10-10 03:20:10 -0700

    installable with setup.py now

--- setup.py
+++ setup.py
@@ -9,8 +9,7 @@ setuptools.setup(
     author_email="m.b.nielsen.1@bham.ac.uk",
     description="A package for querying multiple stellar catalogs",
     long_description=open("README.md").read(),
-    packages=['asterosearch'],
+    packages=setuptools.find_packages(),
     install_requires=open('requirements.txt').read().splitlines(),
-    include_package_data=True,
-    
+    include_package_data=True,   
 )
